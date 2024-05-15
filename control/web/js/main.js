c90fe84b02adb378e1b4a0c414b96280293b8e4e
Josue-Isai-Sanchez-Santos <g17boom@gmail.com>
2024-05-15 13:38:38 -0600

    Segundo cambio en la IP de conexión

--- control/web/js/main.js
+++ control/web/js/main.js
@@ -10,7 +10,7 @@ let mensaje=document.getElementById("mensaje");
 
 function CallApi(estatus){
     // Hacer una petición para un usuario con ID especifico
-axios.get('http://3.17.173.168/iot-car-control/back-end/apis/setRegistro.php?valorEstatus='+estatus)
+axios.get('http://3.16.67.66/iot-car-control/back-end/apis/setRegistro.php?valorEstatus='+estatus)
 .then(function (response) {
   // manejar respuesta exitosa
   console.log(response);
